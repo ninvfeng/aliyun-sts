0d3f09f6ae634ae6e05cf1b778f5eb60c146b4f6
ninvfeng <ninvfeng@qq.com>
2019-04-08 15:05:02 +0800

    fix

--- src/Core/Profile/DefaultProfile.php
+++ src/Core/Profile/DefaultProfile.php
@@ -22,8 +22,8 @@ namespace Sts\Core\Profile;
 use Sts\Core\Auth\Credential;
 use Sts\Core\Auth\ISigner;
 use Sts\Core\Auth\ShaHmac1Signer;
-use Sts\Core\Profile\Endpoint;
-use Sts\Core\Profile\EndpointProvider;
+use Sts\Core\Regions\Endpoint;
+use Sts\Core\Regions\EndpointProvider;
 use Sts\Core\Regions\ProductDomain;
 
 class DefaultProfile implements IClientProfile
